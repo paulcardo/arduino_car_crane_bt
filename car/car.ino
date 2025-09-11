f26e28d8f87ea63aac684e8a74f79d3f0267d25a
PC <paulcardo@hotmail.com>
2025-09-11 14:57:30 -0600

    motor testing

--- car/car.ino
+++ car/car.ino
@@ -5,15 +5,19 @@ Servo servo1;
 Servo servo2;
 
 // car
-#define IN1 2
-#define IN2 3
-#define IN3 4
-#define IN4 5
-//crane
-#define SM1 7  // Servo 1
-#define SML 8  // Servo 2
+#define LM1 7  // motor Driver in1
+#define LM2 5  // motor Driver in2
+#define ENA 6  // (PWM pin) motor Driver enA
 
-SoftwareSerial bt(11, 10);  // RX, TX
+#define ENB 3  // (PWM pin) motor Driver enB
+#define RM1 4  // motor Driver in3
+#define RM2 2  // motor Driver in4
+
+//crane
+#define SM1 8  // Servo 1
+#define SML 13  // Servo 2
+
+SoftwareSerial bt(10, 11);  // RX, TX
 
 /***********************  D E B U G  ****************************/
 // "true" when is Debug, it must show logs in the Serial Monitor.
@@ -25,7 +29,7 @@ char data;
 
 int eje1 = 90;
 int eje2 = 90;
-int maxOpen = 45;
+int maxOpen = 30;
 int maxDown = 40;
 
 void setup() {
@@ -33,18 +37,19 @@ void setup() {
     Serial.begin(9600);
     Serial.println("Hi Setup");
   }
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  //pinMode(EN1, OUTPUT);
-  //pinMode(EN2, OUTPUT);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  //analogWrite(EN1,63);
-  //analogWrite(EN2,63);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
+   // set motor's speed
+  analogWrite(ENA, 75);
+  analogWrite(ENB, 75);
   bt.begin(9600);
 
   servo1.attach(SM1);
@@ -78,18 +83,18 @@ void loop() {
       case 'R':
         right();
         break;
-      case 'S':
+      case 's':
         stopCar();
         break;
       // servo 1
-      case 'D':
+      case 'A':
         if (eje1 < 90 + maxOpen) {
           eje1++;
           log(String(eje1));
           servo1.write(eje1);
         }
         break;
-      case 'I':
+      case 'D':
         if (eje1 > 90 - maxOpen) {
           eje1--;
           log(String(eje1));
@@ -97,20 +102,23 @@ void loop() {
         }
         break;
       // servo 2
-      case 'G':
+      case 'S':
         if (eje2 < 90 + maxDown) {
           eje2++;
           log(String(eje2));
           servo2.write(eje2);
         }
         break;
-      case 'H':
+      case 'W':
         if (eje2 > 90 - maxDown) {
           eje2--;
           log(String(eje2));
           servo2.write(eje2);
         }
         break;
+      case 'Y':
+       resetServos();
+       break;
     }
   }
   if (bt.available() < 0) {
@@ -129,46 +137,46 @@ void log(String msg) {
 
 void forward() {
   log("forward");
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, HIGH);
   delay(20);
 }
 
 void reverse() {
   log("reverse");
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, HIGH);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
   delay(20);
 }
 
 void left() {
   log("left");
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
   delay(20);
 }
 
 void right() {
   log("right");
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, HIGH);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, HIGH);
   delay(20);
 }
 
 void stopCar() {
   log("stop");
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
   delay(20);
 }
 
@@ -179,4 +187,7 @@ void resetServos() {
   servo1.write(90);
   servo2.write(90);
   delay(100);
+  eje1 = 90;
+  eje2 = 90;
+  log(String(eje1) + String(eje2));
 }
